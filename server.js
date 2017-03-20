c09d510d3e78c22431b647f84ceb4b477cbee1b4
Andrii <andriy.vandakurov@gmail.com>
2017-03-20 21:55:49 +0200

    Change server settings according to openshift rules

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const jwt = require('jsonwebtoken')
 const config = require('./config.json')
 
 const socketioJwt = require('socketio-jwt')
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 3002
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -40,14 +40,14 @@ io.sockets
         console.log('decoded_token: ', socket.decoded_token)
 
         console.log(`Server get message from ${socket.decoded_token.username}:`, msg)
-        io.emit('chat message', `User ${socket.decoded_token.username} say: ${msg}`);
+        io.emit('chat message', `User ${socket.decoded_token.username} say: ${msg}`)
       })
   })
   .on('unauthorized', error => {
-    console.log('Users token has expired', error.data);
+    console.log('Users token has expired', error.data)
     // if (error.data.type == "UnauthorizedError" || error.data.code == "invalid_token") {
     //   // redirect user to login page perhaps?
-    //   console.log("User's token has expired");
+    //   console.log("User's token has expired")
     // }
   })
 
