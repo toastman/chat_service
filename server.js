a045363be8ac890316964c4736c7894150068acc
Andrii <andriy.vandakurov@gmail.com>
2017-03-31 18:30:18 +0300

    Serving static files from public folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const routes = require('./routes.js')
 const cors = require('cors')
 const bodyParser = require('body-parser')
